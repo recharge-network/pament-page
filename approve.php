ef3ac6f706026ef63998ac415ad6ea4d06f296e1
recharge-network <myrechargenetwork@gmail.com>
2025-08-29 01:49:19 -0400

    Update approve.php

--- approve.php
+++ approve.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if(!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in']!==true){
+    header("Location: admin-login.php");
+    exit;
+}
+
 $conn = new mysqli("localhost","root","","payments");
 $id = $_GET['id'];
 $conn->query("UPDATE utr_requests SET status='approved' WHERE id=$id");
